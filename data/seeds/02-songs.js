bc01e755f7178bfd239e03545eb5ee346a6f6283
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-05-26 12:59:23 -0700

    blah

--- data/seeds/02-songs.js
+++ data/seeds/02-songs.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('songs').truncate()
+  return knex('songs').del()
     .then(function () {
       // Inserts seed entries
       return knex('songs').insert([
